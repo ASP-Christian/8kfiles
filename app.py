b6c1fbf53fe88c0858d4d9cc5283a8cc0c7d151a
ASP- Christian <128400825+ASP-Christian@users.noreply.github.com>
2024-02-13 11:00:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from selenium.common.exceptions import NoSuchElementException, StaleElementRefer
     ElementClickInterceptedException, TimeoutException
 
 options = webdriver.ChromeOptions()
-options.add_argument("--headless")
+# options.add_argument("--headless")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-gpu")
